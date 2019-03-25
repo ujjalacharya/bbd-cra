26ae8ca6aeddd279f7d87e19b48d2676539a1333
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-03-25 14:33:03 +0545

    initial commit

---
+++ src/Components/Home/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Header from '../shared/Header';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+       <Header />
+        This is home section
+      </div>
+    )
+  }
+}
